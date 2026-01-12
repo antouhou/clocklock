4b8617861c5f5d384ae01f1d494f3bc05a246516
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2026-01-12 13:52:21 -0300

    add prettier and eslint configs

---
+++ eslint.config.js
@@ -0,0 +1,77 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        // Browser globals
+        chrome: 'readonly',
+        console: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+        setInterval: 'readonly',
+        structuredClone: 'readonly',
+        // Node.js globals (for scripts)
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': 'allow-with-description',
+          'ts-ignore': true,
+          'ts-nocheck': true,
+          'ts-check': false,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsparser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      svelte,
+    },
+    rules: {
+      ...svelte.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  prettier,
+];
